ab1b6ab35c83f152372539f08ae0f787a73a32dd
cirius1792 <cirolucio.tecce@gmail.com>
2022-07-22 18:18:24 +0200

    Font selector

--- src/components/InputForm.js
+++ src/components/InputForm.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import classes from "./InputForm.module.css";
+import { SUPPORTED_FONTS } from "./TextComponent";
 const re = new RegExp("^\\w+$");
 const isValid = (value) => {
   if (!value || value.trim() === "" || !re.test(value)) return false;
@@ -19,20 +20,40 @@ const InputForm = (props) => {
   };
   const handleSubmit = (event) => {
     event.preventDefault();
-    if (isValidInput) props.onChange(valueState);
-  }
-
-  console.log(`valid input ${isValidInput}!`)
+    if (isValidInput) props.onNameChange(valueState);
+  };
 
   return (
     <form className={classes.inputForm}>
       <p>
-        <input type="text" value={valueState} onChange={handleChange} placeholder="name" />
-        <input type="submit" value="Done" onClick={handleSubmit} disabled={!isValidInput}/>
+        <input
+          type="text"
+          value={valueState}
+          onChange={handleChange}
+          placeholder="name"
+        />
+        <input
+          type="submit"
+          value="Done"
+          onClick={handleSubmit}
+          disabled={!isValidInput}
+        />
       </p>
-      {touchedState && !isValidInput && <p>
-        <span >Valid Characters: a-zA-Z0-9_</span>
-      </p>}
+      {touchedState && !isValidInput && (
+        <p>
+          <span>Valid Characters: a-zA-Z0-9_</span>
+        </p>
+      )}
+      <p className={classes.fontForm}>
+        <label>Font:</label>
+        <select id="browsers" onChange={(event) => props.onFontChange(event.target.value)}>
+          {SUPPORTED_FONTS.map((font) => (
+            <option key={font} value={font}>
+              {font}
+            </option>
+          ))}
+        </select>
+      </p>
     </form>
   );
 };
--- src/pages/Greetings.js
+++ src/pages/Greetings.js
@@ -1,36 +1,40 @@
-import React, { useState} from "react";
+import React, { useState } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
-import TextComponent, {SUPPORTED_FONTS} from "../components/TextComponent";
+import TextComponent, { SUPPORTED_FONTS } from "../components/TextComponent";
 import InputForm from "../components/InputForm";
 import EditIcon from "../ui/EditIcon";
 import classes from "./Greetings.module.css";
 
 const Greetings = (props) => {
   const [editState, setEditState] = useState(false);
-  const [fontState, setFontState] = useState(SUPPORTED_FONTS[0])
+  const [fontState, setFontState] = useState(SUPPORTED_FONTS[0]);
   const navigate = useNavigate();
   const location = useLocation();
   const queryParams = new URLSearchParams(location.search);
   let name = queryParams.get("name");
-  if(!name) name = "World";
-  const text = `Hello ${name}!`; 
-  
-  const handleChange = (value) => {
-    setEditState(false)
-    navigate(`/greetings?name=${value}`)
+  if (!name) name = "World";
+  const text = `Hello ${name}!`;
+  const handleFontChange = (value) => {
+    setFontState(value);
+  };
+  const handleNameChange = (value) => {
+    setEditState(false);
+    navigate(`/greetings?name=${value}`);
   };
   const handleEditChange = () => setEditState((oldState) => !oldState);
   return (
     <div>
       <div className={classes.mainContainer}>
-        <TextComponent text={text} font={fontState}/>
+        <TextComponent text={text} font={fontState} />
       </div>
       <div className={classes.container}>
-        <EditIcon onClick={handleEditChange}/>
+        <EditIcon onClick={handleEditChange} />
       </div>
-      {editState &&  <div className={classes.container}>
-        <InputForm onChange={handleChange} />
-      </div>}
+      {editState && (
+        <div className={classes.container}>
+          <InputForm onNameChange={handleNameChange} onFontChange={handleFontChange}/>
+        </div>
+      )}
     </div>
   );
 };

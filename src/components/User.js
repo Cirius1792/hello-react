695802fb0e8a501836afa91a66d714b017180f83
cirius1792 <cirolucio.tecce@gmail.com>
2022-07-22 19:30:51 +0200

    About me page

--- src/components/User.js
+++ src/components/User.js
@@ -1,12 +1,21 @@
 import React from "react";
 import ProfilePicture from "./ProfilePicture";
-const myProfilePic  = 'https://media-exp1.licdn.com/dms/image/C4D03AQHa5_BCYbOLFA/profile-displayphoto-shrink_800_800/0/1630404291817?e=1663804800&v=beta&t=K8i-fdv5yGIt788sOJ5iLIGbFfK4P9wnR-N7p7qOLMw'
+import classes from "./User.module.css";
+import linkedinIcon from "../assets/linkedin.svg";
+const myProfilePic =
+  "https://media-exp1.licdn.com/dms/image/C4D03AQHa5_BCYbOLFA/profile-displayphoto-shrink_800_800/0/1630404291817?e=1663804800&v=beta&t=K8i-fdv5yGIt788sOJ5iLIGbFfK4P9wnR-N7p7qOLMw";
 const User = (props) => {
-
   return (
-    <div>
-      <ProfilePicture source={myProfilePic}/>
-      <h1>Ciro Lucio Tecce</h1>
+    <div className={classes.userCard}>
+      <a href={props.linkedin} target="_blank">
+        <ProfilePicture source={myProfilePic} />
+      </a>
+      <h1>{props.fullName}</h1>
+      <h2>{props.title}</h2>
+      <p>{props.description}</p>
+      <a href={props.linkedin} target="_blank">
+        <img className={classes.logo} alt="Linkedin icon" src={linkedinIcon} />{" "}
+      </a>
     </div>
   );
 };

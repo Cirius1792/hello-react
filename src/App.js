d6ca2ab1b752ae093994f1edeed72323afe3e044
cirius1792 <cirolucio.tecce@gmail.com>
2022-07-23 11:44:30 +0200

    About Me page now is llaxy loaded

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
+import React, { Suspense } from "react";
 import Layout from "./layout/Layout";
 import { Routes, Route, Navigate } from "react-router-dom";
 import Greetings from "./pages/Greetings";
-import AboutMe from './pages/AboutMe';
-
+//import AboutMe from './pages/AboutMe';
+const AboutMe = React.lazy(() => import("./pages/AboutMe"));
 function App() {
   return (
     <Layout>
       <Routes>
-        <Route path="/" element={<Navigate to='/greetings' replace />} />
+        <Route path="/" element={<Navigate to="/greetings" replace />} />
         <Route path="/greetings" element={<Greetings />} />
-        <Route path="/about-me" element={<AboutMe />} />
+        <Route
+          path="/about-me"
+          element={
+            <Suspense fallback={<p>Loadng...</p>}>
+              <AboutMe />
+            </Suspense>
+          }
+        />
       </Routes>
     </Layout>
   );
--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -1,15 +1,13 @@
 import User from "../components/User";
 import { useSelector } from "react-redux";
-const whoIAm="I am a curious, passionate and pragmatic Software Engineer. For my entaire "
+const whoIAm="I am a curious, passionate and pragmatic Software Engineer. During my free time, I enjoy studying or experimenting with new topics and this is how this site was born. After several weeks spent studying React, it was time to build a first simple web app to test myself and what I've learned. Have a look at the code and if you have suggestions, or if you spot a bug, please, tell it to me by sending me a message on Linkedin, it will help me improve my skills!  "
 const AboutMe = (props) => {
   const fontState = useSelector((state) => state.font);
 
   return <div  style={{ fontFamily: fontState }}>
     <User fullName="Ciro Lucio Tecce"
     title="Software Engineer"
-    description="I am a curious, passionate and pragmatic Software Engineer. During my free time, I enjoy studying or experimenting with new topics and this is how this site was born. After several weeks spent studying React, it was time to build a first simple web app to test myself and what I've learned.
-
-    Have a look at the code and if you have suggestions, or if you spot a bug, please, tell it to me by sending me a message on Linkedin, it will help me improve my skills!  "
+    description={whoIAm}
     linkedin="https://www.linkedin.com/in/ciro-lucio-tecce-3bb628175/"
     />
   </div>;
